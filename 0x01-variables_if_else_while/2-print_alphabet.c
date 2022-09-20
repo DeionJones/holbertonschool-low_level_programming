a835948c64875f79f076c70c3eec6b4e4416133b
deionjones <5553@holbertonstudents.com>
2022-09-20 07:04:02 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		printf("%c ", ch);
+		ch++;
+	}
+	return (0);
+}
+
