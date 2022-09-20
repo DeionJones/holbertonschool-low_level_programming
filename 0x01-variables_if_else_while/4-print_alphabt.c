5830282fd6d574872de9e7a94c60819c2329b035
deionjones <5553@holbertonstudents.com>
2022-09-20 09:28:22 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char 1;
+
+	1 = 'a';
+	while (1 <= 'z')
+	{
+		if ((1 != 'e') && (1 != 'q'))
+		{
+
+			putchar(1);
+			1++;
+		}
+		else
+		{
+			1++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
