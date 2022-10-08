a0c86cd1746bfce58b65fe5f8d7baa241469af60
deionjones <5553@holbertonstudents.com>
2022-10-08 06:56:26 -0700

    0

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <unistd.h>
+int main(void);
+
+/**
+ * prototype file for last project
+ */
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_re_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+#endif
