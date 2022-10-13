1806fb0d26823476ae5d637cbcd5f04e31c248cf
deionjones <5553@holbertonstudents.com>
2022-10-12 17:05:43 -0700

    0

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * _strncat - function that concatenates two strings.
+ * @dest: string to append by src
+ * @src: string to append by dest
+ * @n: largest number of bytes to append
+ *
+ * Return: address of dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int d, b;
+
+	d = b = 0;
+	while (*(dest + d))
+		d++;
+	while (b < n && *(src + b))
+	{
+		*(dest + d) = *(src + b);
+		d++;
+		b++;
+	}
+	if (b < n)
+		*(dest + d) = *(src + b);
+	return (dest);
+}
