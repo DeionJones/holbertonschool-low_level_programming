5469b8943b9bab1af6800923f5a9c507b53883e9
deionjones <5553@holbertonstudents.com>
2022-10-15 07:30:40 -0700

    lib

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase letters
+ * _isdigit - checks for characters that are digits
  * @c: character to check
- *
  * Return: 0 or 1
  */
 
-int _isupper(int c)
+
+int _isdigit(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
