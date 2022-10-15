5a6182bc790f357158eda5138bbf43e83269bdd9
deionjones <5553@holbertonstudents.com>
2022-10-15 16:32:17 -0700

    dynamic

---
+++ 0x0A-dynamic_libraries/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string.
+ * @dest: appends src
+ * @src: appends dest
+ * @n: integer to append
+ *
+ * Return: address of dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int d;
+
+	d = 0;
+	while (d < n && *(src + d))
+	{
+		*(dest + d) = *(src + d);
+		d++;
+	}
+	while (d < n)
+	{
+		*(dest + d) = '\0';
+		d++;
+	}
+	return (dest);
+}
