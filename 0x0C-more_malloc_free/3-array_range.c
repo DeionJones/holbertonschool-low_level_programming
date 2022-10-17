9d2c076ca2485df623db62a482d30d888094c482
deionjones <5553@holbertonstudents.com>
2022-10-17 12:12:38 -0700

    3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,18 +10,22 @@
  */
 int *array_range(int min, int max)
 {
-	int i, l;
 	int *a;
+	int i, full;
 
+	full = (max - min) + 1;
 	if (min > max)
-		return (NULL);
-        l = max - min + 1;
-	a = malloc(sizeof(int) * 1);
-	if (a == NULL)
-		return (NULL);
-	for (i = 0; i < l; i++, min++)
 	{
-		a[i] = min;
+		return (NULL);
+	}
+	a = malloc(sizeof(int) * full);
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; min <= max; i++)
+	{
+		a[i] = min++;
 	}
 	return (a);
 }
